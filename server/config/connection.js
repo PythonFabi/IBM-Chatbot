39111e5f41c7366ced1f53222c6fb0b981119237
PythonFabi <fabianmelone.fw@gmail.com>
2024-01-30 16:07:00 +0100

    add package.json to server and establish connection with mongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:270'
-)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chat-bot');
+
+module.exports = mongoose.connection;
